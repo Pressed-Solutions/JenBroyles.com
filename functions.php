a5870471d70e76d58609c9d190bebfcb92a8be22
Andrew Minion <andrew@andrewrminion.com>
2015-06-08 16:51:27 -0400

    add woocommerce cart count widget

--- functions.php
+++ functions.php
@@ -247,3 +247,32 @@ genesis_register_sidebar( array(
 	'name'        => __( 'Bottom Sidebar', 'beautiful' ),
 	'description' => __( 'This is the bottom sidebar widget area.', 'beautiful' ),
 ) );
+
+// Add Woocommerce cart count widget
+class WoocommerceCartCount extends WP_Widget {
+    function WoocommerceCartCount() {
+        // Instantiate the parent object
+        parent::__construct( false, 'Woocommerce Cart Count' );
+    }
+
+    function widget( $args, $instance ) {
+        echo '<a class="cart-contents" href="' . WC()->cart->get_cart_url() . '" title="' . _e( 'View your shopping cart' ) . '">' . sprintf (_n( '%d item', '%d items', WC()->cart->cart_contents_count ), WC()->cart->cart_contents_count ) . ' - ' . WC()->cart->get_cart_total() . '</a>';
+    }
+}
+add_action( 'widgets_init', function(){
+    register_widget( 'WoocommerceCartCount' );
+});
+
+// Ensure cart contents update when products are added to the cart via AJAX (place the following in functions.php)
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	ob_start();
+	?>
+	<a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf (_n( '%d item', '%d items', WC()->cart->cart_contents_count ), WC()->cart->cart_contents_count ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a>
+	<?php
+
+	$fragments['a.cart-contents'] = ob_get_clean();
+
+	return $fragments;
+}
