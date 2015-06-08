5a162b496717fb20b6766dac1f0884ab3ab34b7e
Andrew Minion <andrew@andrewrminion.com>
2015-06-08 16:57:09 -0400

    tweak Ajax cart count display

--- functions.php
+++ functions.php
@@ -267,12 +267,5 @@ add_action( 'widgets_init', function(){
 add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
 
 function woocommerce_header_add_to_cart_fragment( $fragments ) {
-	ob_start();
-	?>
-	<a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf (_n( '%d item', '%d items', WC()->cart->cart_contents_count ), WC()->cart->cart_contents_count ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a>
-	<?php
-
-	$fragments['a.cart-contents'] = ob_get_clean();
-
-	return $fragments;
+	return '<a class="cart-contents" href="' . WC()->cart->get_cart_url() . '" title="' . _e( 'View your shopping cart' ) . '"><span class="dashicons dashicons-cart"></span>' . sprintf (_n( '<span class="count">%d</span> item', '<span class="count">%d</span> items', WC()->cart->cart_contents_count ), WC()->cart->cart_contents_count ). '</a>';
 }
