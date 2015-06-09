ed0b0dc77d3a7db2f07bb05acc4c6d994fdd9841
Andrew Minion <andrew@andrewrminion.com>
2015-06-09 14:06:23 -0400

    add wrapping <section>s on the home page

--- home-template.php
+++ home-template.php
@@ -12,13 +12,19 @@ function home_loop() {
 
     echo '<h2 class="primary-header">Recent Blog</h2>';
     echo '<h3 class="secondary-header">Read the latest news from our blog</h3>';
+    echo '<section class="home-blog-wrapper">';
     genesis_custom_loop( array( 'post_type' => 'post' ) );
+    echo '</section>';
 
+    echo '<section class="home-content-wrapper">';
     the_content();
+    echo '</section>';
 
     echo '<h2 class="primary-header">Testimonials</h2>';
     echo '<h3 class="secondary-header">Praises for Jen</h3>';
+    echo '<section class="home-testimonial-wrapper">';
     genesis_custom_loop( array( 'post_type' => 'testimonial' ) );
+    echo '</section>';
 }
 
 // call the rest of the template
