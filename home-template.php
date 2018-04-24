c0cae934804cd8ac40423eb9ecf2f005aafddcd6
Andrew Minion <andrew@andrewrminion.com>
2018-04-24 13:48:13 -0400

    normalize whitespace

--- home-template.php
+++ home-template.php
@@ -8,60 +8,60 @@ remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'home_loop' );
 
 function home_loop() {
-    global $paged;
+	global $paged;
 
-    echo '<h2 class="primary-header">Recent Blog</h2>' . "\n";
-    echo '<h3 class="secondary-header">Read the latest news from our blog</h3>' . "\n";
-    echo '<section class="home-blog-wrapper">' . "\n";
-    // custom loop
-    $blog_args = ( array(
-        'post_type'         => 'post',
-        'posts_per_page'    => 3,
-    ));
-    $blog_query = new WP_Query( $blog_args );
-    while ( $blog_query->have_posts() ) {
-        $blog_query->the_post();
+	echo '<h2 class="primary-header">Recent Blog</h2>' . "\n";
+	echo '<h3 class="secondary-header">Read the latest news from our blog</h3>' . "\n";
+	echo '<section class="home-blog-wrapper">' . "\n";
+	// custom loop
+	$blog_args = ( array(
+		'post_type'         => 'post',
+		'posts_per_page'    => 3,
+	));
+	$blog_query = new WP_Query( $blog_args );
+	while ( $blog_query->have_posts() ) {
+		$blog_query->the_post();
 
-        echo '<article id="' . get_the_ID() . '" class="' . implode( ' ', get_post_class() ) . '" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">' . "\n";
-            echo '<a href="' . get_permalink() . '" title="' . esc_attr( $blog_query->post->post_title ) . '">' . get_the_post_thumbnail( $blog_query->post->ID, 'home_featured' ) . '</a>' . "\n";
-            echo '<div class="wrapper">' . "\n";
-                echo '<h2 class="entry-title" itemprop="headline">' . "\n";
-                    echo '<a href="' . get_permalink() . '" title="' . esc_attr( $blog_query->post->post_title ) . '">' . get_the_title() . '</a>' . "\n";
-                echo '</h2>' . "\n";
-                echo '<p>' . get_the_excerpt() . '</p>' . "\n";
-                echo '<p><a class="read-more" href="' . get_permalink() . '">continue reading &gt;</a></p>';
-            echo '</div>' . "\n";
-        echo '</article>' . "\n";
-    }
-    wp_reset_postdata();
-    echo '</section>' . "\n";
+		echo '<article id="' . get_the_ID() . '" class="' . implode( ' ', get_post_class() ) . '" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">' . "\n";
+			echo '<a href="' . get_permalink() . '" title="' . esc_attr( $blog_query->post->post_title ) . '">' . get_the_post_thumbnail( $blog_query->post->ID, 'home_featured' ) . '</a>' . "\n";
+			echo '<div class="wrapper">' . "\n";
+				echo '<h2 class="entry-title" itemprop="headline">' . "\n";
+					echo '<a href="' . get_permalink() . '" title="' . esc_attr( $blog_query->post->post_title ) . '">' . get_the_title() . '</a>' . "\n";
+				echo '</h2>' . "\n";
+				echo '<p>' . get_the_excerpt() . '</p>' . "\n";
+				echo '<p><a class="read-more" href="' . get_permalink() . '">continue reading &gt;</a></p>';
+			echo '</div>' . "\n";
+		echo '</article>' . "\n";
+	}
+	wp_reset_postdata();
+	echo '</section>' . "\n";
 
-    echo '<section class="home-content-wrapper clearfix">' . "\n";
-    the_post_thumbnail( $post->ID, 'medium' );
-    echo '<div class="wrapper">' . "\n";
-    the_content();
-    echo '</div>' . "\n";
-    echo '</section>' . "\n";
+	echo '<section class="home-content-wrapper clearfix">' . "\n";
+	the_post_thumbnail( $post->ID, 'medium' );
+	echo '<div class="wrapper">' . "\n";
+	the_content();
+	echo '</div>' . "\n";
+	echo '</section>' . "\n";
 
-    echo '<h2 class="primary-header">Testimonials</h2>';
-    echo '<h3 class="secondary-header">Praises for Jen</h3>';
-    echo '<section class="testimonial-wrapper">';
-    // custom loop
-    $blog_args = ( array(
-        'post_type'         => 'testimonial',
-        'posts_per_page'    => 2,
-    ));
-    $blog_query = new WP_Query( $blog_args );
-    while ( $blog_query->have_posts() ) {
-        $blog_query->the_post();
+	echo '<h2 class="primary-header">Testimonials</h2>';
+	echo '<h3 class="secondary-header">Praises for Jen</h3>';
+	echo '<section class="testimonial-wrapper">';
+	// custom loop
+	$blog_args = ( array(
+		'post_type'         => 'testimonial',
+		'posts_per_page'    => 2,
+	));
+	$blog_query = new WP_Query( $blog_args );
+	while ( $blog_query->have_posts() ) {
+		$blog_query->the_post();
 
-        echo '<article id="' . get_the_ID() . '" class="' . implode( ' ', get_post_class() ) . '" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">' . "\n";
-            echo '<p>' . get_the_content() . '</p>' . "\n";
-            echo '<h3 class="entry-title" itemprop="headline">' . get_the_title() . '</h3>' . "\n";
-        echo '</article>' . "\n";
-    }
-    echo '</section>';
-    wp_reset_postdata();
+		echo '<article id="' . get_the_ID() . '" class="' . implode( ' ', get_post_class() ) . '" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">' . "\n";
+			echo '<p>' . get_the_content() . '</p>' . "\n";
+			echo '<h3 class="entry-title" itemprop="headline">' . get_the_title() . '</h3>' . "\n";
+		echo '</article>' . "\n";
+	}
+	echo '</section>';
+	wp_reset_postdata();
 }
 
 // call the rest of the template
